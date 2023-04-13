0074a666241bcfa9b4065c56aee39f15fde5c319
kajmeter <85412627+kajmeter@users.noreply.github.com>
2023-04-13 15:01:59 +0200

    Update texture_mesh.py

--- texture_mesh.py
+++ texture_mesh.py
@@ -1,15 +1,15 @@
 import pymeshlab
 ms = pymeshlab.MeshSet()
 print('loading rasters')
-ms.load_project(['images\BUNDLER.out.bundle.out', 'images\BUNDLER.out.list.txt'])
+ms.load_project('images/focus_simplified.mlp')
 print('-rasters loaded')
 
 print('loading mesh')
-ms.load_new_mesh('mesh.ply')
+ms.load_new_mesh('1.ply')
 print('-mesh loaded')
 
 print('starting texturing')
-ms.compute_texcoord_parametrization_and_texture_from_registered_rasters(texturesize=1024 ,colorcorrection=True)
+ms.compute_texcoord_parametrization_and_texture_from_registered_rasters(texturesize=4096 ,colorcorrection=True)
 print('-texturing done')
 
 
